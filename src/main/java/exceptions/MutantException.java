fa4fdd25702849b60cd6528e0ab21e2b4184650c
Lucas Gioia <lucasgi@synthesis.com.ar>
2020-06-21 15:15:15 -0300

    Primer commit. Esquema básico y una prueba unitaria

---
+++ src/main/java/exceptions/MutantException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+/**
+ * Excepción que se libera ante la detección de un mutante.
+ */
+public class MutantException extends Exception{
+}
---
+++ src/main/java/services/interfaces/MutantService.java
@@ -0,0 +1,9 @@
+package services.interfaces;
+public interface MutantService {
+    /**
+     * Determina si el adn tomado por parámetro corresponde a un mutante o no.
+     * @param dna El adm correspondiente al ser a evaluar.
+     * @return true si es mutante, false en caso contrario.
+     */
+    public boolean isMutant(String[] dna);
+}
