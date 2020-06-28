a8bbccc691a1a0dc68be77fa8a330412bb808b4a
lucasgi <gioialucasf@gmail.com>
2020-06-28 14:11:36 -0300

    Subo una clase faltante

---
+++ src/main/java/com/application/utils/Path.java
@@ -0,0 +1,9 @@
+package com.application.utils;
+
+/**
+ * Clase destinada a mantener las rutas correspondientes a cada recurso.
+ */
+public class Path {
+    public static final String MUTANT = "/mutant/";
+    public static final String STATS = "/stats/";
+}
